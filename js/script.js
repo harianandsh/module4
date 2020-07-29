2e491c770ad54e815265ba68fcf4a58764f3eb93
harianandsh <67405351+harianandsh@users.noreply.github.com>
2020-07-29 11:41:39 +0530

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,33 @@
+var names= ["anand", "json","jhanson"];
+
+for (var i = 0; i < names.length; i++) {
+
+var firstLetter = names[i].charAt(0).toLowerCase();
+
+if (firstLetter === 'j') {
+    console.log("hello j"+ names[i]);
+  } 
+
+
+
+
+  else {
+
+  	console.log("goodbye "+names[i]);
+    
+  }
+
+
+
+
+}
+
+
+
+
+
+
+
+
+
+
